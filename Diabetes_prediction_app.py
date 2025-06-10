e7ab55a0a4d292059e56582accc70e62a78e5a4c
air73 <air735599@gmail.com>
2025-06-10 16:56:58 +0530

    second change

--- Diabetes_prediction_app.py
+++ Diabetes_prediction_app.py
@@ -4,6 +4,16 @@ import numpy as np
 import pickle 
 from sklearn.preprocessing import StandardScaler
 
+
+#Connecting the code with mongodb to store the data 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://Anurag:Anurag1234@cluster0.bagh3cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["diabetes"]
+collection = db["diabetes_prediction"]
+
+
 # Writing the function to load the model 
 def load_model():
     with open("diabetes_prediction.pkl","rb") as file:
@@ -30,7 +40,7 @@ def main():
     st.write("Enter your data to get a prediction for your diabetes risk")
 
     # Now we are going to create fields where user can fill the data
-    Pregnancies = st.number_input("Pregnancies", min_value=1, max_value=100, value=35)
+    Pregnancies = st.number_input("Pregnancies", min_value=0, max_value=100, value=0)
     Glucose = st.number_input("Glucose", min_value=10.0, max_value=50.0, value=25.0)  # Fixed: was "sex" instead of "BMI"
     BloodPressure = st.number_input("Blood Pressure", min_value=40, max_value=300, value=120)
     SkinThickness = st.number_input("SkinThickness", min_value=0, max_value=500, value=200)
@@ -59,7 +69,8 @@ def main():
         # Imagine you have a big box of toys (that's called user_data), and each toy has a name tag on it. But sometimes the name tags are written in a special computer language that's hard to read.
         # This code is like having a helpful friend who goes through your toy box and rewrites all the name tags in regular English so everyone can understand them better.
         user_data = {key: int(value) if isinstance(value , np.integer) else float(value) if isinstance(value , np.floating) else value for key , value in user_data.items()}   
-                                                                                                                                    
+
+        collection.insert_one(user_data)                                                                                                                      
 #To execute the above url function line 
 if __name__ == "__main__" :
     main()
